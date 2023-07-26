d79e32f4b23b5ac70857f855d46d586759ca37d5
MrJeris <mrjeris@yandex.ru>
2023-07-26 16:13:12 +0300

    Initial Commit

---
+++ TestCase/TestCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCaseApp.swift
+//  TestCase
+//
+//  Created by Ruslan Magomedov on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
